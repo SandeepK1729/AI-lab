500d338fb36fef56e10c295f32ff9756a3c1f37d
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-08-18 11:34:26 +0545

    Update jug solver.py

--- week - 4/jug solver.py
+++ week - 4/jug solver.py
@@ -1,33 +1,33 @@
 from collections import defaultdict
 
-
-jug1, jug2, aim = 4, 3, 2
-
-
+jug_1, jug_2, aim = 4, 3, 2
 visited = defaultdict(lambda: False)
 
-def waterJugSolver(amt1, amt2):
+def waterJugSolver(amount_1, amount_2):
 
-	if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
-		print(amt1, amt2)
+	if (amount_1 == aim and amount_2 == 0) or (amount_2 == aim and amount_1 == 0):
+		print(amount_1, amount_2)
 		return True
 	
-	if visited[(amt1, amt2)] == False:
-		print(amt1, amt2)
-	
-		visited[(amt1, amt2)] = True
-	
-		return (waterJugSolver(0, amt2) or
-				waterJugSolver(amt1, 0) or
-				waterJugSolver(jug1, amt2) or
-				waterJugSolver(amt1, jug2) or
-				waterJugSolver(amt1 + min(amt2, (jug1-amt1)),
-				amt2 - min(amt2, (jug1-amt1))) or
-				waterJugSolver(amt1 - min(amt1, (jug2-amt2)),
-				amt2 + min(amt1, (jug2-amt2))))
-
-	else:
+	if visited[(amount_1, amount_2)]:
 		return False
+		
+	visited[(amount_1, amount_2)] = True
+	print(amount_1, amount_2)
+	
+	return (
+      waterJugSolver(0, amount_2) or
+      waterJugSolver(amount_1, 0) or
+      waterJugSolver(jug_1, amount_2) or
+      waterJugSolver(amount_1, jug_2) or
+      waterJugSolver(amount_1 + min(amount_2, (jug_1-amount_1)), 
+                     amount_2 - min(amount_2, (jug_1-amount_1))
+                     ) or
+      waterJugSolver(amount_1 - min(amount_1, (jug_2-amount_2)),
+                     amount_2 + min(amount_1, (jug_2-amount_2))
+                     )
+	)
+
 
 print("Steps: ")
 
