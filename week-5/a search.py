5571fa73faf9ab673a2d52867b1b40bf294d47b4
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-08-23 21:54:43 +0530

    detailing view

--- week-5/a search.py
+++ week-5/a search.py
@@ -1,27 +1,38 @@
-class A_star : 
-  def __init__(self, a, H) : 
-    self.H = H 
-    self.a = a 
-    
-  def find_a_star(self, start, end) :
-    open_list = [(start, self.H[start])]
-    
-    while len(open_list) > 0 : 
-      print ( open_list )
-      c_node, c_cost = open_list.pop(0) 
-      if c_node == end : 
-        return (c_cost - self.H[end])
-      for node, cost in self.a[c_node] : 
-        cost += c_cost - self.H[c_node] + self.H[node]
-        open_list.append((node, cost))
-      open_list.sort() 
+class A_star:
+    """Class to find less cost path using A* star Informed Search algorithm"""
 
-if __name__ == "__main__" : 
-  H = { 'A' : 1, 'B' : 1,'C' : 1,'D' : 1 }
-  A= {
-    'A': [('B', 1), ('C', 3), ('D', 7)],
-    'B': [('D', 5)],
-    'C': [('D', 12)]
-  }
-  a = A_star(A, H)
-  print(a.find_a_star('A', 'C'))
+    def __init__(self, adjancency_matrix, H):
+        self.H = H
+        self.adjancency_matrix = adjancency_matrix
+
+    def find_a_star(self, start, end):
+        open_list = [(start, self.H[start])]
+
+        while len(open_list) > 0:
+
+            print(open_list)
+            current_node, current_cost = open_list.pop(0)
+
+            if current_node == end:
+                return (current_cost - self.H[end])
+
+            for node, cost in self.adjancency_matrix[c_node]:
+
+                cost += (current_cost - self.H[current_node]) + self.H[node]
+                open_list.append((node, cost))
+
+            open_list.sort()
+
+        return "No path exist"
+
+
+if __name__ == "__main__":
+    H = {'A': 1, 'B': 1, 'C': 1, 'D': 1}
+    A = {
+        'A': [('B', 1), ('C', 3), ('D', 7)],
+        'B': [('D', 5)],
+        'C': [('D', 12)]
+    }
+	
+    a = A_star(A, H)
+    print(a.find_a_star('A', 'C'))
