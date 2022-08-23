e64c62500272194aa0721e0d633fc8c46947c337
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-08-23 15:34:41 +0530

    Done with a* star search

--- week-5/a* search.py
+++ week-5/a* search.py
@@ -1,165 +1,27 @@
-from collections import deque
+class A_star : 
+  def __init__(self, a, H) : 
+    self.H = H 
+    self.a = a 
+    
+  def find_a_star(self, start, end) :
+    open_list = [(start, self.H[start])]
+    
+    while len(open_list) > 0 : 
+      print ( open_list )
+      c_node, c_cost = open_list.pop(0) 
+      if c_node == end : 
+        return (c_cost - self.H[end])
+      for node, cost in self.a[c_node] : 
+        cost += c_cost - self.H[c_node] + self.H[node]
+        open_list.append((node, cost))
+      open_list.sort() 
 
-class Graph:
-    def __init__(self, adjac_lis):
-        self.adjac_lis = adjac_lis
-
-    def get_neighbors(self, v):
-        return self.adjac_lis[v]
-
-    def h(self, n):
-        H = {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1
-        }
-
-        return H[n]
-
-    def a_star_algorithm(self, start, stop):
-
-        open_lst = set([start])
-        closed_lst = set([])
-
-        poo = {}
-        poo[start] = 0
-
-        par = {}
-        par[start] = start
-
-        while len(open_lst) > 0:
-            n = None
-
-            for v in open_lst:
-                if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
-                    n = v;
-
-            if n == None:
-                print('Path does not exist!')
-                return None
-
-            if n == stop:
-                reconst_path = []
-
-                while par[n] != n:
-                    reconst_path.append(n)
-                    n = par[n]
-
-                reconst_path.append(start)
-
-                reconst_path.reverse()
-
-                print('Path found: {}'.format(reconst_path))
-                return reconst_pathfrom collections import deque
-
-class Graph:
-    def __init__(self, adjac_lis):
-        self.adjac_lis = adjac_lis
-
-    def get_neighbors(self, v):
-        return self.adjac_lis[v]
-
-    def h(self, n):
-        H = {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1
-        }
-
-        return H[n]
-
-    def a_star_algorithm(self, start, stop):
-
-        open_lst = set([start])
-        closed_lst = set([])
-
-        poo = {}
-        poo[start] = 0
-
-        par = {}
-        par[start] = start
-
-        while len(open_lst) > 0:
-            n = None
-
-            for v in open_lst:
-                if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
-                    n = v;
-
-            if n == None:
-                print('Path does not exist!')
-                return None
-
-            if n == stop:
-                reconst_path = []
-
-                while par[n] != n:
-                    reconst_path.append(n)
-                    n = par[n]
-
-                reconst_path.append(start)
-
-                reconst_path.reverse()
-
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
-
-            for (m, weight) in self.get_neighbors(n):
-        
-                if m not in open_lst and m not in closed_lst:
-                    open_lst.add(m)
-                    par[m] = n
-                    poo[m] = poo[n] + weight
-
-                else:
-                    if poo[m] > poo[n] + weight:
-                        poo[m] = poo[n] + weight
-                        par[m] = n
-
-                        if m in closed_lst:
-                            closed_lst.remove(m)
-                            open_lst.add(m)
-
-            open_lst.remove(n)
-            closed_lst.add(n)
-
-        print('Path does not exist!')
-        return None
-adjac_lis = {
+if __name__ == "__main__" : 
+  H = { 'A' : 1, 'B' : 1,'C' : 1,'D' : 1 }
+  A= {
     'A': [('B', 1), ('C', 3), ('D', 7)],
     'B': [('D', 5)],
     'C': [('D', 12)]
-}
-graph1 = Graph(adjac_lis)
-graph1.a_star_algorithm('A', 'D')
-
-            for (m, weight) in self.get_neighbors(n):
-        
-                if m not in open_lst and m not in closed_lst:
-                    open_lst.add(m)
-                    par[m] = n
-                    poo[m] = poo[n] + weight
-
-                else:
-                    if poo[m] > poo[n] + weight:
-                        poo[m] = poo[n] + weight
-                        par[m] = n
-
-                        if m in closed_lst:
-                            closed_lst.remove(m)
-                            open_lst.add(m)
-
-            open_lst.remove(n)
-            closed_lst.add(n)
-
-        print('Path does not exist!')
-        return None
-adjac_lis = {
-    'A': [('B', 1), ('C', 3), ('D', 7)],
-    'B': [('D', 5)],
-    'C': [('D', 12)]
-}
-graph1 = Graph(adjac_lis)
-graph1.a_star_algorithm('A', 'D')
+  }
+  a = A_star(A, H)
+  print(a.find_a_star('A', 'C'))
